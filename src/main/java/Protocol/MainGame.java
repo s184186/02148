82516e8be7f1c59c36ad6615d10c4dae2d5c5814
Frederik Lyhne Andersen <freddybla17@gmail.com>
2020-01-10 15:52:22 +0100

    Protocol files

---
+++ src/main/java/Protocol/MainGame.java
@@ -0,0 +1,27 @@
+package Protocol;
+
+import org.jspace.Space;
+
+import java.util.Arrays;
+
+public class MainGame {
+
+    private String[] players;
+    private int[] teams;
+    private int version;
+    private Space game;
+    private String host;
+
+    public MainGame(String host, String[] players, int[] teams, int version, Space game){
+        this.players = players;
+        this.teams = teams;
+        this.version = version;
+        this.game = game;
+        this.host = host;
+    }
+
+    public void startGame(){
+        System.out.println("Players: " + Arrays.toString(players));
+        System.out.println("Teams: " + Arrays.toString(teams));
+    }
+}
