72eb3a321e66806f4be81b2cfdf7b932abfa7e51
s184186 <s184186@student.dtu.dk>
2020-01-23 20:54:49 +0100

    added icon to alle stages

--- src/main/java/Lobby/ConnectToGameController.java
+++ src/main/java/Lobby/ConnectToGameController.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import java.io.IOException;
 
@@ -62,6 +63,7 @@ public class ConnectToGameController {
 
             lobbyStage.setScene(lobbyScene);
             lobbyStage.setResizable(false);
+            lobbyStage.getIcons().add(new Image(getClass().getResource("/icon.png").toExternalForm()));
             lobbyStage.show();
         } else {
             connectionFailedLabel.setText("Unable to connect. Either lobby is full or username is in use");
--- src/main/java/Lobby/SetupGameController.java
+++ src/main/java/Lobby/SetupGameController.java
@@ -8,6 +8,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import org.jspace.SequentialSpace;
 import java.io.IOException;
@@ -90,6 +91,7 @@ public class SetupGameController {
 
         lobbyStage.setScene(lobbyScene);
         lobbyStage.setResizable(false);
+        lobbyStage.getIcons().add(new Image(getClass().getResource("/icon.png").toExternalForm()));
         lobbyStage.show();
 
     }
